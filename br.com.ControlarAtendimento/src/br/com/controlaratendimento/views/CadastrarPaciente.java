7cdb4fc3eb095260ba72aea15f2c0a4608386adb
Bruno Vian <bruno.vian@edu.unipar.br>
2022-11-17 19:40:06 -0300

    ajustes gerais

--- br.com.ControlarAtendimento/src/br/com/controlaratendimento/views/CadastrarPaciente.java
+++ br.com.ControlarAtendimento/src/br/com/controlaratendimento/views/CadastrarPaciente.java
@@ -234,7 +234,7 @@ public class CadastrarPaciente extends javax.swing.JFrame {
                     .addComponent(idadePaciente, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel12))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(cancelarPaciente, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(salvarPaciente, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(34, Short.MAX_VALUE))
